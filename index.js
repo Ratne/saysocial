8c569073318989a5f7f2a3a69102005e988b8ac4
ratne <iacomidavide@gmail.com>
2023-05-30 10:48:50 +0200

    update

--- index.js
+++ index.js
@@ -75,28 +75,119 @@ const callTagActivecampaign = (contact, tag) => {
 
 const userExist = (contact) => {
 
-
-  axios.get(`${baseUrl}/contacts?filters[email]=${contact.email}`, {
-        headers: {
-            'content-type': 'text/json',
-            "Api-Token": apiKey
-        }}).then(user => {
-
-       if (user.data.contacts[0].id){
-            // qui fare chiamate altre
-       }
-       else {
-           // fare chiamata nuovo contatto e tutti i crismi
-         }
+  return new Promise((resolve, reject) => {
+        axios.get(`${baseUrl}/contacts?filters[email]=${contact.email}`, {
+            headers: {
+                'content-type': 'text/json',
+                "Api-Token": apiKey
+            }}).then(user => {
+            if (user.data.contacts.length) {
+                resolve(user.data.contacts[0].id)
+            }
+            else {
+                resolve(false)
+            }
         }).catch(errorNotFound => {
             console.log(errorNotFound)
-    })
+            reject(false)
+        })
+  })}
+
+
+const upddateNewContact = (contact, id) => {
+
+
+        let dataUpdateContact = {
+            "contact": {
+                "email": contact.email,
+                "firstName": contact.name,
+                "lastName": contact.surname,
+                "phone": contact.phone,
+                "fieldValues": [
+                    {
+                        "field": "3",
+                        "value": contact.city.toString()
+                    },
+                    {
+                        "field": "33",
+                        "value": contact.message.toString()
+                    },
+                    {
+                        "field": "1",
+                        "value": contact.language.toString()
+                    }
+                ]
+            }
+        }
+        axios.put(`${baseUrl}/contacts/${id}`, dataUpdateContact, {
+            headers: {
+                'content-type': 'text/json',
+                "Api-Token": apiKey
+            }
+        }).then(newContact => {
+            const dataArray = ["37","40", ...(contact.gdpr === true ? ["38"] : [])]
+
+            scrollTags(newContact.data.contact.id, dataArray)
+
+        })
+
+}
+
+const updateNewProject = (contact, id) => {
+console.log('entrato in aggiornamento')
+        let dataUpdateContact = {
+            "contact": {
+                "email": contact.email,
+                "firstName": contact.name,
+                "lastName": contact.surname,
+                "phone": contact.phone,
+                "fieldValues": [
+                    {
+                        "field": "3",
+                        "value": contact.city.toString()
+                    },
+                    {
+                        "field": "27",
+                        "value": contact.message.toString()
+                    },
+                    {
+                        "field": "26",
+                        "value": contact.budget.toString()
+                    },
+                    {
+                        "field": "25",
+                        "value": contact.know_us.toString()
+                    },
+                    {
+                        "field": "13",
+                        "value": contact.business_name.toString()
+                    },
+                    {
+                        "field": "1",
+                        "value": contact.language.toString()
+                    }
+                ]
+            }
+        }
+        axios.put(`${baseUrl}/contacts/${id}`, dataUpdateContact, {
+            headers: {
+                'content-type': 'text/json',
+                "Api-Token": apiKey
+            }
+        }).then(newContact => {
+                const dataArray = ["37","39", ...(contact.gdpr === true ? ["38"] : [])]
+
+                scrollTags(newContact.data.contact.id, dataArray)
+
+            }
+        ).catch(newContactError => {
+            console.log('inserimento nuovo contatto errato ' +newContactError)
+        })
 
 }
 
 
 const newContact = (contact)=> {
-
     let dataNewContact = {
         "contact": {
             "email": contact.email,
@@ -190,31 +281,63 @@ const newProject = (contact)=> {
 
 
 
-app.post('/newcontact', (req, res) => {
-    const isOk = contactSchema.validate(req.body)
-    if (isOk.error) return res.status(400).send(isOk.error.details[0].message);
-    else {
-        newContact(req.body)
-        res.status(200).json({message: 'Contatto Inserito'})}
-})
+// app.post('/newcontact', (req, res) => {
+//     const isOk = contactSchema.validate(req.body)
+//     if (isOk.error) return res.status(400).send(isOk.error.details[0].message);
+//     else {
+//         newContact(req.body)
+//         res.status(200).json({message: 'Contatto Inserito'})}
+// })
 
-app.post('/newproject', (req, res) => {
-        const isOk = projectSchema.validate(req.body)
-        if (isOk.error) return res.status(400).send(isOk.error.details[0].message);
-        else {
-            newProject(req.body)
-            res.status(200).json({message: 'Contatto Inserito'})
-        }
-
-})
+// app.post('/newproject', (req, res) => {
+//         const isOk = projectSchema.validate(req.body)
+//         if (isOk.error) return res.status(400).send(isOk.error.details[0].message);
+//         else {
+//             newProject(req.body)
+//             res.status(200).json({message: 'Contatto Inserito'})
+//         }
+//
+// })
 
 app.get('/', (req, res) => {
     res.status(200).send('Online')
 })
 
-app.post('/test', (req, res) => {
-    userExist(req.body)
-    res.status(200).send(req.body)
+app.post('/newproject', async (req, res) => {
+    const isOk = projectSchema.validate(req.body)
+    if (isOk.error) return res.status(400).json({message: isOk.error.details[0].message});
+    else {
+        const user = await userExist(req.body)
+        if (user) {
+            console.log('utente trovato con id', user)
+            updateNewProject(req.body, user)
+            res.status(200).json({message: 'Contatto Inserito'})
+        }
+        else if (user === false){
+            console.log('utente NON trovato ', user)
+            newProject(req.body)
+            res.status(200).json({message: 'Contatto Inserito'})
+        }
+    }
+
+
+})
+app.post('/newcontact', async (req, res) => {
+    const isOk = contactSchema.validate(req.body)
+    if (isOk.error) return res.status(400).json({message: isOk.error.details[0].message});
+    else {
+        const user = await userExist(req.body)
+        if (user) {
+            upddateNewContact(req.body, user)
+            res.status(200).json({message: 'Contatto Inserito'})
+        }
+        else if (user === false){
+            newContact(req.body)
+            res.status(200).json({message: 'Contatto Inserito'})
+        }
+    }
+
+
 })
 
 app.listen(port, () => {
